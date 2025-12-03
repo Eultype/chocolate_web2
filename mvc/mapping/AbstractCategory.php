e5dccc0fe0028fba67b33f2c531ee541e333162e
Mykyta Horban <masarash12345@gmail.com>
2025-12-03 15:31:05 +0100

    CategoryMapping

---
+++ mvc/mapping/AbstractCategory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace model\mapping;
+
+abstract class AbstractCategory extends AbstractMapping
+{
+    // Cette classe peut contenir des méthodes communes à toutes les catégories si nécessaire
+}
