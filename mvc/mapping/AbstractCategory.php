0fc7817cb16b074796070ddb119ffec7fb0269ee
Mykyta Horban <masarash12345@gmail.com>
2025-12-04 09:13:16 +0100

    AbstractCategory

--- mvc/mapping/AbstractCategory.php
+++ mvc/mapping/AbstractCategory.php
@@ -2,7 +2,73 @@
 
 namespace model\mapping;
 
-abstract class AbstractCategory extends AbstractMapping
+abstract class AbstractCategory
 {
-    // Cette classe peut contenir des méthodes communes à toutes les catégories si nécessaire
+    /**
+     * Définit la valeur de category_id
+     * @param int $category_id
+     * @return self
+     */
+    private $category_id;
+    public function setCategoryId(int $category_id): self
+    {
+        
+    }
+
+    /**
+     * Récupère la valeur de category_title
+     * @return string
+     */
+    public function getCategoryTitle(): string
+    {
+        
+    }
+
+    /**
+     * Définit la valeur de category_title
+     * @param string $category_title
+     * @return self
+     */
+    public function setCategoryTitle(string $category_title): self
+    {
+        
+    }
+
+    /**
+     * Récupère la valeur de category_slug
+     * @return string
+     */
+    public function getCategorySlug(): string
+    {
+       
+    }
+
+    /**
+     * Définit la valeur de category_slug
+     * @param string $category_slug
+     * @return self
+     */
+    public function setCategorySlug(string $category_slug): self
+    {
+        
+    }
+
+    /**
+     * Récupère la valeur de category_desc
+     * @return string|null
+     */
+    public function getCategoryDesc(): ?string
+    {
+        
+    }
+
+    /**
+     * Définit la valeur de category_desc
+     * @param string|null $category_desc
+     * @return self
+     */
+    public function setCategoryDesc(?string $category_desc): self
+    {
+        
+    }
 }
